ad259b60ae95e9f1212213d2f254fe7b15dd7be9
Patryk Czarnik <patryk@patrykczarnik.net>
2020-03-01 09:35:37 +0100

    Starting to clean up and write down Python routines.

---
+++ src/python/video_scripts/__init__.py
@@ -0,0 +1,12 @@
+# (c) Patryk Czarnik
+# Distributed under MIT License. See LICENCE file in the root directory for details.
+
+# A library of my video processing utilities using ffmpeg.
+# The set of tools is specially addressed to processing of car camera videos.
+
+
+### global constants and default settings ###
+
+
+
+
